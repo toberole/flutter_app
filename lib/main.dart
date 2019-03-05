f824a1144fe063519664ad3e5f8f1c4ccaa16cb7
zhouwei <zhouwei215664@sogou-inc.com>
2019-03-05 19:04:46 +0800

    创建listview

--- lib/main.dart
+++ lib/main.dart
@@ -139,17 +139,56 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class RandomWords extends StatefulWidget{
+class RandomWords extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return new RandomWordsState();
   }
 }
 
-class RandomWordsState extends State<RandomWords>{
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
-    return new Text(wordPair.asPascalCase);
+//    final wordPair = new WordPair.random();
+//    return new Text(wordPair.asPascalCase);
+    // 在Dart语言中使用下划线前缀标识符，会强制其变成私有的
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Startup Name Generator"),),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+      // 对于每个建议的单词对都会调用一次itemBuilder，然后将单词对添加到ListTile行中
+      // 在偶数行，该函数会为单词对添加一个ListTile row.
+      // 在奇数行，该函数会添加一个分割线widget，来分隔相邻的词对。
+
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return new Divider();
+
+        // 语法 "i ~/ 2" 表示i除以2，但返回值是整形（向下取整），比如i为：1, 2, 3, 4, 5
+        // 时，结果为0, 1, 1, 2, 2， 这可以计算出ListView中减去分隔线后的实际单词对数量
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
   }
 }
